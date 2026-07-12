7a6b808f7cd24383016e33638f309aaae87c3c7f
rohan-ak43 <akrohan437@gmail.com>
2026-07-12 10:14:41 +0530

    977. Squares of sorted array

---
+++ square_sorted_array.py
@@ -0,0 +1,16 @@
+# Solution 1
+class Solution(object):
+    def sortedSquares(self, nums):
+        square = [i * i for i in nums]
+        square.sort()
+        return square
+
+
+# Solution 2
+class Solution(object):
+    def sortedSquares(self, nums):
+        for i in range(len(nums)):
+            nums[i] = nums[i] * nums[i]
+        nums.sort()
+        return nums
+        
