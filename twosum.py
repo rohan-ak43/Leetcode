e01f351ac97b01a5f95bb6faf30d71ca73eee6eb
rohan-ak43 <akrohan437@gmail.com>
2026-07-19 21:09:56 +0530

    1. Two Sum - Hashmap

--- twosum.py
+++ twosum.py
@@ -1,7 +1,18 @@
+# Solution 1 - Brute Force Method
 class Solution(object):
     def twoSum(self, nums, target):
         for i in range(len(nums)):
             for j in range(i):
                 if nums[i] + nums[j] == target:
                     return i,j
-        
+
+# Solution 2 - Hashmap (Optimal solution)  
+class Solution(object):
+    def twoSum(self, nums, target):
+        hashm = {}
+        for i,num in enumerate(nums):
+            com = target - num 
+            if com in hashm:
+                return [hashm[com],i]
+            hashm[num] = i 
+              
