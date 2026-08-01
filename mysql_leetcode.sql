7657b2d9f2d31d82aab97fcf5ec310af2b068345
rohan-ak43 <akrohan437@gmail.com>
2026-08-01 16:01:38 +0530

    183. Customers Who Never Order - MySQL

--- mysql_leetcode.sql
+++ mysql_leetcode.sql
@@ -1,3 +1,6 @@
 #175. Combine Two Tables
 
 select p.firstName,p.lastName,c.city,c.state from person p left join Address c on p.personId = c.personId;
+
+# 183. Customers Who Never Order
+select name as Customers from Customers where id not in (select customerId from Orders);
