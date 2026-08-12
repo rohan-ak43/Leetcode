0509cefbd2aac8f241c4edfb75226f97382a2797
rohan-ak43 <akrohan437@gmail.com>
2026-08-12 17:41:43 +0530

    1890. The Latest Login in 2020

--- mysql_leetcode.sql
+++ mysql_leetcode.sql
@@ -4,3 +4,9 @@ select p.firstName,p.lastName,c.city,c.state from person p left join Address c o
 
 # 183. Customers Who Never Order
 select name as Customers from Customers where id not in (select customerId from Orders);
+
+# 1890. The Latest Login in 2020
+select user_id, max(time_stamp) as last_stamp 
+FROM Logins 
+where time_stamp >= '2020-01-01 00:00:00' and time_stamp < '2021-01-01 00:00:00' 
+GROUP BY user_id;
