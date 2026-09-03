3ab42bb3a9729e77f2b05399da50abf1d6486ef2
rohan-ak43 <akrohan437@gmail.com>
2026-09-03 22:41:00 +0530

    217.Contains Duplicate

--- ContainsDuplicate.py
+++ ContainsDuplicate.py
@@ -7,3 +7,12 @@ class Solution(object):
                 return True
             hashset.add(i)
         return False
+
+# Solution 2
+class Solution(object):
+    def containsDuplicate(self, nums):
+        nums.sort()
+        for i in range(1,len(nums)):
+            if nums[i] == nums[i-1]:
+                return True
+        return False
