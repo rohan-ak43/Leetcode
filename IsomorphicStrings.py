151ab54123739b74a923c55240f83b4eb9caccfa
rohan-ak43 <akrohan437@gmail.com>
2026-09-09 20:42:41 +0530

    205. Isomorphic Strings

--- IsomorphicStrings.py
+++ IsomorphicStrings.py
@@ -1,12 +1,11 @@
-# Time Complexity : O(n)
 class Solution(object):
     def isIsomorphic(self, s, t):
-        map1 = []
-        map2 = []
-        for idx in s:
-            map1.append(s.index(idx))
-        for idx in t:
-            map2.append(t.index(idx))
-        if map1 == map2:
+        list1 = []
+        list2 = []
+        for i in s:
+            list1.append(s.index(i))
+        for j in t:
+            list2.append(t.index(j))
+        if list1 == list2:
             return True
         return False
