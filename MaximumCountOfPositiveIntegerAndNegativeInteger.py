e4fbd27fb0e3778735eb217c48d70de68f64d4b2
rohan-ak43 <akrohan437@gmail.com>
2026-09-12 23:29:15 +0530

    2529.Maximum Count of Positive Integer and Negative Integer

--- MaximumCountOfPositiveIntegerAndNegativeInteger.py
+++ MaximumCountOfPositiveIntegerAndNegativeInteger.py
@@ -1,3 +1,4 @@
+# Solution 1
 class Solution(object):
     def maximumCount(self, nums):
         negcount = 0
@@ -13,3 +14,17 @@ class Solution(object):
             return poscount
         else:
             return negcount
+
+# Solution 2
+class Solution(object):
+    def maximumCount(self, nums):
+        negcount = 0
+        poscount = 0
+        for i in nums:
+            if i == 0:
+                continue
+            elif i<0:
+                negcount += 1
+            else:
+                poscount += 1
+        return max(negcount,poscount)
