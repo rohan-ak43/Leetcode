91cd82567031826a336531cddf85b9a1c60ed4f7
rohan-ak43 <akrohan437@gmail.com>
2026-09-16 19:40:11 +0530

    434.Number of Segments in a String

--- NumberOfSegmentsInAString.py
+++ NumberOfSegmentsInAString.py
@@ -3,3 +3,12 @@ class Solution(object):
     def countSegments(self, s):
         liost = s.split()
         return len(liost)
+
+# Solution 2
+class Solution(object):
+    def countSegments(self, s):
+        count = 0
+        for i in range(len(s)):
+            if s[i] != ' ' and (i==0 or s[i-1]==' '):
+                count += 1
+        return count
